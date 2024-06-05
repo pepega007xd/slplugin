796f8545e518d7632f732a4c2885a87551e6b179
pepega007xd <brablect@gmail.com>
2024-06-05 15:06:39 +0200

    add example code for report

--- examples/linked_list.c
+++ examples/linked_list.c
@@ -1,22 +1,55 @@
 #include "stdlib.h"
 
-typedef struct LinkedList {
-        struct LinkedList *next;
+typedef struct List {
+        struct List *next;
         int data;
-} LinkedList;
+} List;
 
 int main() {
-    LinkedList *list = malloc(sizeof(LinkedList));
-    LinkedList *start = list;
-    list->data = 4;
-    list->next = NULL;
+    List *start = malloc(sizeof(List));
+    if (start == NULL)
+        return 1;
 
-    int nondeterministic;
-    while (nondeterministic) {
-        list->next = malloc(sizeof(LinkedList));
-        list->data = 4;
-        list = list->next;
+    { // construct a linked list of unknown size
+        List *list = start;
+        list->next = NULL;
+
+        int nondeterministic;
+        while (nondeterministic) {
+            List *next = malloc(sizeof(List));
+            if (next == NULL)
+                return 1;
+
+            list->next = next;
+            list = list->next;
+        }
+
+        list->next = NULL;
     }
 
-    list->next = NULL;
+    { // walk to the end of the list
+        List *list = start;
+
+        while (list != NULL) {
+            List *next = NULL;
+            next = list->next;
+
+            list->data = 42;
+            list = next;
+        }
+    }
+
+    { // free the list
+        List *list = start;
+
+        while (list != NULL) {
+            List *next = NULL;
+            next = list->next;
+
+            free(list);
+            list = next;
+        }
+    }
+
+    start->next = NULL;
 }
